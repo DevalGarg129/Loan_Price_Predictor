ccf01ebedd414870a214b01101d9248da6b6b3d3
Deval Garg <devalgarg978@gmail.com>
2026-01-02 15:49:13 +0530

    deleting

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from .data_preprocessing import load_and_preprocess_data, encode_categoricals
-from .feature_engineering import add_features
-from .utils import scale_features
-from .model_training import train_model 
-from .evaluation import evaluate_model
-
-from src import load_and_preprocess_data
--- src/feature_engineering.py
+++
@@ -1,5 +0,0 @@
-def add_features(df):
-    df['Total_Income'] = df['ApplicantIncome'] + df['CoapplicantIncome']
-    df['EMI'] = df['LoanAmount'] / df['Loan_Amount_Term']
-    df['Balance_Income'] = df['Total_Income'] - (df['EMI'] * 1000)
-    return df
